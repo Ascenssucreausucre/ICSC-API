0310b7f2aa1abfd6315c8271ee922167ce5ee235
flol <amiotflorian33@gmail.com>
2025-05-12 16:03:44 +0200

    v.1 finalized

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,15 +1,16 @@
-// config/sequelize.js
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(
-  process.env.LOCAL_DB_NAME,
-  process.env.LOCAL_DB_USER,
-  process.env.LOCAL_DB_PASSWORD,
+  isProduction ? process.env.PROD_DB_NAME : process.env.LOCAL_DB_NAME,
+  isProduction ? process.env.PROD_DB_USER : process.env.LOCAL_DB_USER,
+  isProduction ? process.env.PROD_DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
   {
-    host: process.env.LOCAL_DB_HOST,
+    host: isProduction ? process.env.PROD_DB_HOST : process.env.LOCAL_DB_HOST,
     dialect: "mysql",
-    logging: false,
+    logging: !isProduction, // active les logs seulement en dev
   }
 );
 
