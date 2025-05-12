4c378ddcfd323f12043f68ad0f8cb774c2a11ce2
flol <amiotflorian33@gmail.com>
2025-05-12 15:52:26 +0200

    save

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.LOCAL_DB_NAME,
+  process.env.LOCAL_DB_USER,
+  process.env.LOCAL_DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.LOCAL_DB_HOST,
     dialect: "mysql",
     logging: false,
   }
